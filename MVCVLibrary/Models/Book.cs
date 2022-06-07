0853c6735697756c423397e495e1c6fe1e929d76
Patrick Gannon <gannon.patrick01@gmail.com>
2022-06-06 22:58:17 -0400

    Add project files.

---
+++ MVCVLibrary/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCVLibrary.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; } // Location of the book in the library
+        public string Author { get; set; }
+    }
+}
---
+++ MVCVLibrary/Models/SeedData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCVLibrary.Data;
+
+namespace MVCVLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return; // If there's books in the db, then just return
+                }
+                context.Book.AddRange(
+                    new Book { Title = "Tiny C# Projects", CallNumber = "AXD 2029" },
+                    new Book { Title = "Guns of August", CallNumber = "AOI 2038" }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
